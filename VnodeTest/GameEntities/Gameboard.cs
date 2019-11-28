92aadbba6feaf992ed172324afe7de2cb565f373
Eisenschenk <o.eisenschenk@imassense.de>
2019-11-28 11:04:02 +0100

    currently implementing algebraic notaion: missing castling, check, checkmate

--- VnodeTest/GameEntities/Gameboard.cs
+++ VnodeTest/GameEntities/Gameboard.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace VnodeTest.GameEntities
@@ -9,7 +10,22 @@ namespace VnodeTest.GameEntities
     public class Gameboard
     {
         public Tile[] Board = new Tile[64];
-
+        public bool Promotion;
+        public bool GameOver;
+        public PieceColor CurrentPlayerColor = PieceColor.White;
+        public Tile Selected;
+        //public (int X, int Y) PositionXY
+        //{
+        //    get
+        //    {
+        //        return (X: Board[])
+        //    }
+        //}
+        public Tile this[int x, int y]
+        {
+            get => Board[y * 8 + x];
+            set => Board[y * 8 + x] = value;
+        }
         public Gameboard()
         {
             for (int index = 0; index < 64; index++)
@@ -48,5 +64,164 @@ namespace VnodeTest.GameEntities
         }
 
         public Gameboard Copy() => new Gameboard(Board.Select(t => t.Copy()));
+
+        public bool TryCastling(Tile start, Tile target)
+        {
+            //some enemy piece might be standing next to the king, rooks potentialmoves dont prevent castling ...
+            if (target.ContainsPiece)
+                if (target.Piece is Rook && start.Piece is King
+                && !target.Piece.HasMoved && !start.Piece.HasMoved
+                && start.Piece.Color == target.Piece.Color
+                && (target.Piece.GetStraightLines(this).Contains(start.Piece.Position + 1)
+                    || target.Piece.GetStraightLines(this).Contains(start.Piece.Position - 1)))
+                {
+                    int direction = 1;
+                    if (start.Piece.Position > target.Piece.Position)
+                        direction *= -1;
+
+                    Board[start.Position + 2 * direction].Piece = start.Piece;
+                    Board[start.Position + 2 * direction].Piece.Position = start.Position + 2 * direction;
+                    Board[start.Position + direction].Piece = target.Piece;
+                    Board[start.Position + direction].Piece.Position = start.Position + direction;
+
+                    Board[start.Position].Piece = null;
+                    Board[target.Position].Piece = null;
+                    Selected = null;
+                    return true;
+                }
+            return false;
+        }
+
+        public bool TryMove(Tile start, Tile target)
+        {
+            if (!start.Piece.GetValidMovements(this).Contains(target.Position) || OwnKingIsCheckedAfterMove(start, target))
+                return false;
+            MovePiece(start, target);
+            Selected = null;
+
+            return true;
+        }
+
+        public void CheckForGameOver()
+        {
+            foreach (Tile tile in Board.Where(t => t.ContainsPiece && t.Piece.Color == CurrentPlayerColor))
+                foreach (int potentialmove in tile.Piece.GetValidMovements(this))
+                    if (!OwnKingIsCheckedAfterMove(tile, Board[potentialmove]))
+                        return;
+            GameOver = true;
+        }
+
+        private void MovePiece(Tile start, Tile target)
+        {
+            target.Piece = start.Piece;
+            target.Piece.Position = target.Position;
+            start.Piece = null;
+        }
+
+        public void ChangeCurrentPlayer()
+        {
+            if (CurrentPlayerColor == PieceColor.White)
+                CurrentPlayerColor = PieceColor.Black;
+            else
+                CurrentPlayerColor = PieceColor.White;
+        }
+
+        public void TryEnablePromotion(Tile tile)
+        {
+            if (tile.Piece is Pawn && (tile.Piece.Position > 55 || tile.Piece.Position < 7))
+            {
+                Selected = tile;
+                Promotion = true;
+            }
+        }
+
+        private bool OwnKingIsCheckedAfterMove(Tile source, Tile targetTile)
+        {
+            Tile start = source.Copy();
+            Tile target = targetTile.Copy();
+            var futureGameBoard = this.Copy();
+            futureGameBoard.Board[target.Position].Piece = start.Piece;
+            futureGameBoard.Board[target.Position].Piece.Position = target.Position;
+            futureGameBoard.Board[start.Position].Piece = null;
+            var kingSameColorPosition = futureGameBoard.Board
+                .Where(t => t.ContainsPiece && t.Piece.Color == start.Piece.Color && t.Piece is King)
+                .First().Piece.Position;
+            var enemyPieces = futureGameBoard.Board.Where(x => x.ContainsPiece && x.Piece.Color != start.Piece.Color);
+
+            if (enemyPieces.SelectMany(t => t.Piece.GetValidMovements(futureGameBoard)).Contains(kingSameColorPosition))
+                return true;
+            return false;
+        }
+        public bool TryANtoMyN(string notation)
+        {
+            var match = Regex.Match(notation, "^(?<piece>[KQRNB]?)(?<sourceX>[a-h]?)(?<sourceY>[1-8]?)(?<captures>x?)(?<destination>[a-h][1-8])(?<promotion>(=[QRNB])?)(?<check>\\+?)(?<mate>#?)$",
+                    RegexOptions.Singleline & RegexOptions.ExplicitCapture);
+            //_pieces where !=empty && correct color
+            var _pieces = Board.Where(p => p.ContainsPiece && p.Piece.Color == CurrentPlayerColor);
+
+            if (match.Value == "")
+                //_pieces where correct PieceType
+                _pieces = _pieces.Where(v => (match.Groups["piece"].Value switch
+                {
+                    "K" => v.Piece is King,
+                    "Q" => v.Piece is Queen,
+                    "R" => v.Piece is Rook,
+                    "N" => v.Piece is Knight,
+                    "B" => v.Piece is Bishop,
+                    _ => v.Piece is BasePiece
+                }));
+            //_pieces where correct sourceX position if its specified
+            _pieces = _pieces.Where(x =>
+            match.Groups["sourceX"].Value != null
+            ? x.Piece.PositionXY.X == ParseStringXToInt(match.Groups["sourceX"].Value)
+            : x.ContainsPiece);
+
+            //_pieces where correct sourceY position if its specified
+            _pieces = _pieces.Where(y =>
+            match.Groups["sourceY"].Value != null
+            ? y.Piece.PositionXY.Y == ParseStringYToInt(match.Groups["sourceY"].Value)
+            : y.ContainsPiece);
+
+            //_pieces where potentialmoves include destination
+            string _destination = match.Groups["destination"].Value;
+            var destination = this[ParseStringXToInt(_destination), ParseStringYToInt(_destination)];
+            _pieces = _pieces.Where(d => d.Piece.GetValidMovements(this).Contains(destination.Position));
+
+            //promotion
+            if (match.Groups["promotion"].Value != null)
+            {
+                TryMove(_pieces.Single(), destination);
+                Board[destination.Position].Piece = match.Groups["promotion"].Value switch
+                {
+                    "=Q" => new Queen(destination.Position, CurrentPlayerColor),
+                    "=R" => new Rook(destination.Position, CurrentPlayerColor),
+                    "=N" => new Knight(destination.Position, CurrentPlayerColor),
+                    "=B" => new Bishop(destination.Position, CurrentPlayerColor),
+                    _ => default
+                };
+                return true;
+            }
+
+            if (TryMove(_pieces.Single(), destination))
+                return true;
+            return false;
+        }
+
+        private int ParseStringXToInt(string input)
+        {
+            var c = input[0];
+            if (c < 'a' || c > 'h')
+                throw new Exception("out of bounds X");
+            return c - 'a';
+        }
+        private int ParseStringYToInt(string input)
+        {
+            var c = input[1];
+            if (c < '1' || c > '8')
+                throw new Exception("out of bounds Y");
+            return c - '1';
+        }
+
     }
 }
+
--- VnodeTest/GameboardController.cs
+++ VnodeTest/GameboardController.cs
@@ -11,10 +11,8 @@ namespace VnodeTest
     public class GameboardController
     {
         public GameEntities.Gameboard GameBoard;
-        private GameEntities.Tile Selected;
-        private bool Promotion;
-        private bool GameOver;
-        private GameEntities.PieceColor CurrentPlayerColor = GameEntities.PieceColor.White;
+
+
 
         public GameboardController()
         {
@@ -23,10 +21,10 @@ namespace VnodeTest
 
         public VNode Render()
         {
-            if (GameOver)
+            if (GameBoard.GameOver)
                 return RenderGameOver();
             return Div(
-                Promotion ? RenderPromotionSelection() : RenderBoard(GameBoard)
+                GameBoard.Promotion ? RenderPromotionSelection() : RenderBoard(GameBoard)
             );
         }
         //remember Enumerable range
@@ -41,7 +39,7 @@ namespace VnodeTest
         private VNode RenderTile(GameEntities.Tile tile)
         {
             return Div(
-                tile.Style & (tile == Selected ? Styles.Selected : tile.BorderStyle),
+                tile.Style & (tile == GameBoard.Selected ? Styles.Selected : tile.BorderStyle),
                 () => Select(tile),
                 tile.ContainsPiece ? Text(tile.Piece.Sprite, Styles.FontSize3) : null
             );
@@ -50,7 +48,7 @@ namespace VnodeTest
         private VNode RenderGameOver()
         {
             string winner;
-            switch (CurrentPlayerColor)
+            switch (GameBoard.CurrentPlayerColor)
             {
                 case GameEntities.PieceColor.Black: winner = "White"; break;
                 case GameEntities.PieceColor.White: winner = "Black"; break;
@@ -63,10 +61,10 @@ namespace VnodeTest
         private VNode RenderPromotionSelection()
         {
             List<GameEntities.Tile> promotionSelect = new List<GameEntities.Tile>();
-            promotionSelect.Add(new GameEntities.Tile(new GameEntities.Rook(0, Selected.Piece.Color), 0));
-            promotionSelect.Add(new GameEntities.Tile(new GameEntities.Knight(1, Selected.Piece.Color), 1));
-            promotionSelect.Add(new GameEntities.Tile(new GameEntities.Bishop(2, Selected.Piece.Color), 2));
-            promotionSelect.Add(new GameEntities.Tile(new GameEntities.Queen(3, Selected.Piece.Color), 3));
+            promotionSelect.Add(new GameEntities.Tile(new GameEntities.Rook(0, GameBoard.Selected.Piece.Color), 0));
+            promotionSelect.Add(new GameEntities.Tile(new GameEntities.Knight(1, GameBoard.Selected.Piece.Color), 1));
+            promotionSelect.Add(new GameEntities.Tile(new GameEntities.Bishop(2, GameBoard.Selected.Piece.Color), 2));
+            promotionSelect.Add(new GameEntities.Tile(new GameEntities.Queen(3, GameBoard.Selected.Piece.Color), 3));
             return Div(
                 Styles.M2,
                 Text($"Select Piece you want the Pawn to be promoted to.", Styles.FontSize1p5),
@@ -77,117 +75,28 @@ namespace VnodeTest
         private void Select(GameEntities.Tile target)
         {
             // eigener Select für Promotion bzw. RenderTile mit Onclick param
-            if (Promotion == true)
+            if (GameBoard.Promotion == true)
             {
-                GameBoard.Board[Selected.Position].Piece = target.Piece;
-                GameBoard.Board[Selected.Position].Piece.Position = Selected.Position;
-                Selected = null;
-                Promotion = false;
+                GameBoard.Board[GameBoard.Selected.Position].Piece = target.Piece;
+                GameBoard.Board[GameBoard.Selected.Position].Piece.Position = GameBoard.Selected.Position;
+                GameBoard.Selected = null;
+                GameBoard.Promotion = false;
                 return;
             }
-            if (Selected == null && target.ContainsPiece /*&& target.Piece.Color == CurrentPlayerColor*/)
-                Selected = target;
-            else if (Selected == target)
-                Selected = null;
-            else if (Selected != null)
-                if (TryMove(Selected, target))
-                {
-                    TryEnablePromotion(target);
-                    ChangeCurrentPlayer();
-                    CheckForGameOver();
-                }
-        }
-
-        private bool TryCastling(GameEntities.Tile start, GameEntities.Tile target)
-        {
-            //some enemy piece might be standing next to the king, rooks potentialmoves dont prevent castling ...
-            if (target.ContainsPiece)
-                if (target.Piece is GameEntities.Rook && start.Piece is GameEntities.King
-                && !target.Piece.HasMoved && !start.Piece.HasMoved
-                && start.Piece.Color == target.Piece.Color
-                && (target.Piece.GetStraightLines(GameBoard).Contains(start.Piece.Position + 1)
-                    || target.Piece.GetStraightLines(GameBoard).Contains(start.Piece.Position - 1)))
-                {
-                    int direction = 1;
-                    if (start.Piece.Position > target.Piece.Position)
-                        direction *= -1;
-
-                    GameBoard.Board[start.Position + 2 * direction].Piece = start.Piece;
-                    GameBoard.Board[start.Position + 2 * direction].Piece.Position = start.Position + 2 * direction;
-                    GameBoard.Board[start.Position + direction].Piece = target.Piece;
-                    GameBoard.Board[start.Position + direction].Piece.Position = start.Position + direction;
-
-                    GameBoard.Board[start.Position].Piece = null;
-                    GameBoard.Board[target.Position].Piece = null;
-                    Selected = null;
-                    return true;
-                }
-            return false;
-        }
-
-        private bool TryMove(GameEntities.Tile start, GameEntities.Tile target)
-        {
-            if (!TryCastling(start, target))
+            if (GameBoard.Selected == null && target.ContainsPiece /*&& target.Piece.Color == CurrentPlayerColor*/)
+                GameBoard.Selected = target;
+            else if (GameBoard.Selected == target)
+                GameBoard.Selected = null;
+            else if (GameBoard.Selected != null)
             {
-                if (!start.Piece.GetValidMovements(GameBoard).Contains(target.Position) || OwnKingIsCheckedAfterMove(start, target))
-                    return false;
-                MovePiece(start, target);
-                Selected = null;
-                return true;
-            }
-            return true;
-        }
-
-        private void CheckForGameOver()
-        {
-            foreach (GameEntities.Tile tile in GameBoard.Board.Where(t => t.ContainsPiece && t.Piece.Color == CurrentPlayerColor))
-                foreach (int potentialmove in tile.Piece.GetValidMovements(GameBoard))
-                    if (!OwnKingIsCheckedAfterMove(tile, GameBoard.Board[potentialmove]))
-                        return;
-            GameOver = true;
-        }
-
-        private void MovePiece(GameEntities.Tile start, GameEntities.Tile target)
-        {
-            target.Piece = start.Piece;
-            target.Piece.Position = target.Position;
-            start.Piece = null;
-        }
-
-        private void ChangeCurrentPlayer()
-        {
-            if (CurrentPlayerColor == GameEntities.PieceColor.White)
-                CurrentPlayerColor = GameEntities.PieceColor.Black;
-            else
-                CurrentPlayerColor = GameEntities.PieceColor.White;
-        }
-
-        private void TryEnablePromotion(GameEntities.Tile tile)
-        {
-            if (tile.Piece is GameEntities.Pawn && (tile.Piece.Position > 55 || tile.Piece.Position < 7))
-            {
-                Selected = tile;
-                Promotion = true;
+                if (!GameBoard.TryCastling(GameBoard.Selected, target))
+                {
+                    GameBoard.TryMove(GameBoard.Selected, target);
+                    GameBoard.TryEnablePromotion(target);
+                    GameBoard.ChangeCurrentPlayer();
+                    GameBoard.CheckForGameOver();
+                }
             }
         }
-
-        private bool OwnKingIsCheckedAfterMove(GameEntities.Tile source, GameEntities.Tile targetTile)
-        {
-            GameEntities.Tile start = source.Copy();
-            GameEntities.Tile target = targetTile.Copy();
-            var futureGameBoard = GameBoard.Copy();
-            futureGameBoard.Board[target.Position].Piece = start.Piece;
-            futureGameBoard.Board[target.Position].Piece.Position = target.Position;
-            futureGameBoard.Board[start.Position].Piece = null;
-            var kingSameColorPosition = futureGameBoard.Board
-                .Where(t => t.ContainsPiece && t.Piece.Color == start.Piece.Color && t.Piece is GameEntities.King)
-                .First().Piece.Position;
-            var enemyPieces = futureGameBoard.Board.Where(x => x.ContainsPiece && x.Piece.Color != start.Piece.Color);
-
-            if (enemyPieces.SelectMany(t => t.Piece.GetValidMovements(futureGameBoard)).Contains(kingSameColorPosition))
-                return true;
-            return false;
-        }
-
     }
 }
